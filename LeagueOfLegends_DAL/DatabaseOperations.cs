ff5c1e0a9a2f65ad955005ec223def6516c75df6
Matix072 <60871296+Matix072@users.noreply.github.com>
2020-05-22 15:12:13 +0200

    Add files via upload
    project

---
+++ LeagueOfLegends_DAL/DatabaseOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace LeagueOfLegends_DAL
+{
+    public static class DatabaseOperations
+    {
+        public static List<Friendship> OphalenFriendList()
+        {
+            using (Data_r0704742Entities entities = new Data_r0704742Entities())
+            {
+                var query = entities.Friendship
+                    .Include(x => x.Speler)
+                    .Include(x => x.Speler1);
+                return query.ToList();
+            }
+        }
+
+        public static List<Skin> OphalenSkins()
+        {
+            using (Data_r0704742Entities entities = new Data_r0704742Entities())
+            {
+                var query = entities.Skins;
+                return query.ToList();
+            }
+        }
+
+       
+    }
+}
